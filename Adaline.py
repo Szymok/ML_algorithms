b397555b3185f91bb9490594a044e1254ecfdfb3
Szymon Kowalewski <szymonkowal019@gmail.com>
2022-01-21 21:19:11 +0100

    _shuffle method

--- Adaline.py
+++ Adaline.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-class AdalineGD(object):
+
+class AdalineSGD(object):
     """ADAptive LInear NEuron classifier.
 
     Parameters
@@ -9,21 +10,31 @@ class AdalineGD(object):
         Learning rate (between 0.0 and 1.0)
     n_iter : int
         Passes over the training dataset.
+    shuffle : bool (default: True)
+        Shuffles training data every epoch if True to prevent
+        cycles.
     random_state : int
-        Random number generator seed for random weight initialization.
+        Random number generator seed for random weight
+        initialization.
+
 
     Attributes
     -----------
     w_ : 1d-array
         Weights after fitting.
     cost_ : list
-        Sum-of-squares cost function value in each epoch.
+        Sum-of-squares cost function value averaged over all
+        training examples in each epoch.
+
 
     """
 
-    def __init__(self, eta=0.01, n_iter=50, random_state=1):
+    def __init__(self, eta=0.01, n_iter=10,
+                 shuffle=True, random_state=None):
         self.eta = eta
         self.n_iter = n_iter
+        self.w_initialized = False
+        self.shuffle = shuffle
         self.random_state = random_state
 
     def fit(self, X, y):
@@ -32,9 +43,8 @@ class AdalineGD(object):
         Parameters
         ----------
         X : {array-like}, shape = [n_examples, n_features]
-            Training vectors, where n_examples
-            is the number of examples and
-            n_features is the number of features.
+            Training vectors, where n_examples is the number of
+            examples and n_features is the number of features.
         y : array-like, shape = [n_examples]
             Target values.
 
@@ -43,20 +53,49 @@ class AdalineGD(object):
         self : object
 
         """
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01,
-                              size=1 + X.shape[1])
+        self._initialize_weights(X.shape[1])
         self.cost_ = []
-
         for i in range(self.n_iter):
-            net_input = self.net_input(X)
-            output = self.activation(net_input)
-            errors = (y - output)
-            self.w_[1:] += self.eta * X.T.dot(errors)
-            self.w_[0] += self.eta * errors.sum()
-            cost = (errors ** 2).sum() / 2.0
-            self.cost_.append(cost)
+            if self.shuffle:
+                X, y = self._shuffle(X, y)
+            cost = []
+            for xi, target in zip(X, y):
+                cost.append(self._update_weights(xi, target))
+            avg_cost = sum(cost) / len(y)
+            self.cost_.append(avg_cost)
         return self
+
+    def partial_fit(self, X, y):
+        """Fit training data without reinitializing the weights"""
+        if not self.w_initialized:
+            self._initialize_weights(X.shape[1])
+        if y.ravel().shape[0] > 1:
+            for xi, target in zip(X, y):
+                self._update_weights(xi, target)
+        else:
+            self._update_weights(X, y)
+        return self
+
+    def _shuffle(self, X, y):
+        """Shuffle training data"""
+        r = self.rgen.permutation(len(y))
+        return X[r], y[r]
+
+    def _initialize_weights(self, m):
+        """Initialize weights to small random numbers"""
+        self.rgen = np.random.RandomState(self.random_state)
+        self.w_ = self.rgen.normal(loc=0.0, scale=0.01,
+                                   size=1 + m)
+        self.w_initialized = True
+
+    def _update_weights(self, xi, target):
+        """Apply Adaline learning rule to update the weights"""
+        output = self.activation(self.net_input(xi))
+        error = (target - output)
+        self.w_[1:] += self.eta * xi.dot(error)
+        self.w_[0] += self.eta * error
+        cost = 0.5 * error ** 2
+        return cost
 
     def net_input(self, X):
         """Calculate net input"""
